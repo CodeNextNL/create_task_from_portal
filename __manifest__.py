cb3568b56f066d6c3c00bb19c3fa19f8ddd36a67
Wytze Jan Riedstra <wjriedstra@gmail.com>
2019-05-03 09:57:05 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) codeNext
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Portal create task as customer',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'codeNext',
+    'website': 'https://www.codenext.nl',
+    'category': 'Hidden',
+    'depends': [
+        'portal'
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+}
